c995e65ee9898254e3572e42e2eeb722a225047e
gaurelior <132467593+gaurelior@users.noreply.github.com>
2024-09-02 09:43:52 -0300

    Add files via upload

---
+++ src/components/ImagensDiversos.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="containerImagem">
+    <img style=" width: 100%;" src="@/assets/cafegosto.jpg" alt="imagem 1">
+    </div>
+    <div class="divImagemMeiaTela">
+        <img src="@/assets/cafethe.jpg" alt="imagem 2">
+        <img src="@/assets/cafemaq.jpg" alt="imagem 3">  
+      </div>
+     
+    </template>
+    
+    <script>
+    export default {
+        name: 'ImagensDiversos',
+        props: {
+            msg: String 
+        }
+    }
+    </script>
+    
+    <!--Add "scoped" atribute to limite css to this component only-->
+    <style scoped></style>
